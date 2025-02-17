d90b1bebbe3d0dc4945c37b695692f15ce5ba5c7
Brissou <brice.eliasse@gmail.com>
2025-02-17 10:26:36 +0100

    Update route.js

--- app/api/basic/route.js
+++ app/api/basic/route.js
@@ -2,42 +2,37 @@ import { NextResponse } from "next/server";
 import Data from "@/models/Data";
 import connect from "@/models/connect";
 
-export const revalidate = 43200;
+export const revalidate = 0;
 
 export async function POST(req) {
-    await connect();
+  await connect();
 
-    const { data } = await req.json();
+  const { data } = await req.json();
 
-    const lastEntry = await Data.findOne()
-        .sort({ date: -1 }) // Trie par date de création décroissante
-        .exec().lean();
+  const lastEntry = await Data.findOne().sort({ date: -1 }).exec();
 
-    // Nombre de séances depuis la dernière fois
-    const seancesSince = lastEntry ? data.totalVisits - lastEntry.sport : data.totalVisits;
+  // Nombre de séances depuis la dernière fois
+  const seancesSince = lastEntry
+    ? data.totalVisits - lastEntry.sport
+    : data.totalVisits;
 
-    const visits = data.recentVisits.slice(0, seancesSince).reverse();
+  const visits = data.recentVisits.slice(0, seancesSince).reverse();
 
+  for (let i = 1; i <= seancesSince; i++) {
+    const startOfDay = new Date(visits[i - 1].timestamp);
+    startOfDay.setUTCHours(0, 0, 0, 0);
+    const endOfDay = new Date(visits[i - 1].timestamp);
+    endOfDay.setUTCHours(23, 59, 59, 999);
 
-    for (let i = 1; i <= seancesSince; i++) {
-
-        const startOfDay = new Date(visits[i - 1].timestamp);
-        startOfDay.setUTCHours(0, 0, 0, 0);
-        const endOfDay = new Date(visits[i - 1].timestamp);
-        endOfDay.setUTCHours(23, 59, 59, 999);
-
-        await Data.updateOne({
-            date: {
-                $gte: startOfDay,
-                $lte: endOfDay
-            }
+    await Data.updateMany(
+      {
+        date: {
+          $gte: startOfDay,
         },
-            { $inc: { sport: 1 } })
+      },
+      { $inc: { sport: 1 } }
+    );
+  }
 
-    }
-
-
-    return NextResponse.json({ result: data });
+  return NextResponse.json({ result: data });
 }
-
-
